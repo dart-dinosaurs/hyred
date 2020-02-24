df6fb4fdc885f3cd3b4cb055aecf4c026ab380aa
skeshavaa <s.keshavaa@gmail.com>
2020-02-23 19:45:18 -0500

    im trying dood :(

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import './widgets/jobCards.dart';
-=======
 import 'package:main/screens/widgets/jobCard.dart';
->>>>>>> 35676c106ca99710200a035ad416d7bff10c5ed9
 
 class Explore extends StatelessWidget {
   @override
--- lib/screens/widgets/jobCards.dart
+++ lib/screens/widgets/jobCards.dart
@@ -4,6 +4,11 @@ import './jobCard.dart';
 class JobCards extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new JobCard();
+    return new Scaffold(
+      body: new Column(children: <Widget>[
+        new JobCard(),
+        new JobCard(),
+      ],)
+    );
   }
 }
