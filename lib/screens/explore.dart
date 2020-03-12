ac0f20231fa8966fc4279277ca249c00fb91dd21
skeshavaa <s.keshavaa@gmail.com>
2020-03-12 12:45:54 -0400

    added appbar

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -11,6 +11,14 @@ class Explore extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return new Scaffold(
+      appBar: AppBar(
+        title: Row(children: <Widget>[
+          Text("Explore Jobs!     ")
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        backgroundColor: Theme.of(context).accentColor,
+      ),
       body: new ListView.builder(
         itemBuilder: (context, index) => new JobCard(job[index]),
         itemCount: job.length,
