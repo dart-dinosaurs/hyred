9234a6db48ce9e3022bc10a57ee50130a7babd70
skeshavaa <s.keshavaa@gmail.com>
2020-03-29 23:03:58 -0400

    basics of history page done

--- lib/screens/seeker-screens/historyDetails.dart
+++ lib/screens/seeker-screens/historyDetails.dart
@@ -8,24 +8,73 @@ class HistoryDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<String> _headings = [
+      'Job Title:',
+      'Employer:',
+      'Compensation:',
+      'Location:',
+      'Language:'
+    ];
+    Map _details = job.data['jobDetails'];
+    List<String> _info = [
+      _details['name'],
+      _details['employer'],
+      "\$" + _details['salary'].toString() + "/hr",
+      _details['location'],
+      _details['language']
+    ];
+
     return (Scaffold(
         body: Stack(
       children: <Widget>[
         ListView(
           children: <Widget>[
-           
             Image(
               image: NetworkImage(job.data['jobDetails']['image']),
               width: MediaQuery.of(context).size.width,
+            ),
+            Container(
+              height: 10,
+            ),
+            Text(
+              "Job Details",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey),
+            ),
+            Container(
+              height: 20,
+            ),
+            Container(
+              child: Row(
+                children: <Widget>[
+                  _column(context, _headings, CrossAxisAlignment.start, TextStyle(fontSize: 15)),
+                  _column(context, _info, CrossAxisAlignment.end, TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                ],
+                crossAxisAlignment: CrossAxisAlignment.center,
+              ),
+              padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.1, 0, MediaQuery.of(context).size.width * 0.1, 0),
             )
           ],
         ),
-         Container(
-              margin:
-                  new EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-              child: new BackButton(color: Colors.white),
-            ),
+        Container(
+          margin: new EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+          child: new BackButton(color: Colors.white),
+        ),
       ],
     )));
   }
 }
+
+Container _column(
+    BuildContext context, List<String> headings, CrossAxisAlignment alignment, TextStyle style) {
+  return (Container(
+      child: Column(
+        children: headings.map((f) => Container(child: Text(f, style: style,),padding: EdgeInsets.fromLTRB(0, 0, 0, 10),),).toList(),
+        crossAxisAlignment: alignment,),
+      width: MediaQuery.of(context).size.width *0.4,
+  ));
+}
+
