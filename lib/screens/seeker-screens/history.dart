7aeab1d48a05195d406b5b42942df213c360f369
skeshavaa <s.keshavaa@gmail.com>
2020-04-05 21:00:01 -0400

    lol

--- lib/screens/seeker-screens/history.dart
+++ lib/screens/seeker-screens/history.dart
@@ -1,8 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:main/screens/widgets/data.dart';
 import 'package:main/screens/widgets/historyCard.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:main/screens/widgets/jobCard.dart';
 import 'package:provider/provider.dart';
 
 class History extends StatelessWidget {
@@ -12,7 +11,7 @@ class History extends StatelessWidget {
 
     var jobs = Provider.of<QuerySnapshot>(context);
 
-    List<DocumentSnapshot> _widgets = jobs.documents.where((doc) => doc.data['jobDetails']['name'].contains('Cashier')).toList();
+    List<DocumentSnapshot> _widgets = jobs.documents;
     List<Widget> _actual = _widgets.map((doc) => HistoryCard(doc)).toList();
 
     return(
