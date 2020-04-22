b6b23b1735642e2c25fb07224ccd7d422c93ad26
skeshavaa <s.keshavaa@gmail.com>
2020-04-22 01:53:32 -0400

    fixed job card

--- lib/screens/widgets/jobCard.dart
+++ lib/screens/widgets/jobCard.dart
@@ -38,27 +38,109 @@ class JobCard extends StatelessWidget {
       onTap: () => Navigator.of(context).push(new PageRouteBuilder(
         pageBuilder: (_, __, ___) => new DetailPage(job),
       )),
-      child: Container(
-        child: Row(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  bottomLeft: Radius.circular(20),
-                  topRight: Radius.circular(50),
-                  bottomRight: Radius.circular(50)),
-              child: Image(
-                  image: new NetworkImage(job['jobDetails']['image']),
-                  height: 140,
-                  width: 150,
-                  fit: BoxFit.cover),
-            ),
-            Column(children: <Widget>[
-              Container(
-                child: Text(job['jobDetails']['name'], style: TextStyle(fontSize: 20),),
-                margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
-              ),
-              Container(
+      child: Stack(
+        children: <Widget>[
+          _info(context, job, _date),
+          _pic(context, job),
+        ],
+        
+      )
+      // child: Container(
+      //   child: Row(
+      //     children: <Widget>[
+      //       ClipRRect(
+      //         borderRadius: BorderRadius.only(
+      //             topLeft: Radius.circular(20),
+      //             bottomLeft: Radius.circular(20),
+      //             topRight: Radius.circular(50),
+      //             bottomRight: Radius.circular(50)),
+      //         child: Image(
+      //             image: new NetworkImage(job['jobDetails']['image']),
+      //             height: 140,
+      //             width: 150,
+      //             fit: BoxFit.cover),
+      //       ),
+      //       Column(children: <Widget>[
+      //         Container(
+      //           child: Text(job['jobDetails']['name'], style: TextStyle(fontSize: 20),),
+      //           margin: EdgeInsets.fromLTRB(10, 10, 0, 0),
+      //         ),
+      //         Container(
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: <Widget>[
+      //               Row(
+      //                 children: <Widget>[
+      //                   Icon(
+      //                     Icons.attach_money,
+      //                     size: 18,
+      //                   ),
+      //                   Text(job['jobDetails']['salary'].toString() + "/hr"),
+      //                 ],
+      //               ),
+      //               Row(
+      //                 children: <Widget>[
+      //                   Icon(Icons.location_on, size: 20),
+      //                   Text(job['jobDetails']['location'])
+      //                 ],
+      //               ),
+      //             ],
+      //           ),
+      //           margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      //           width: MediaQuery.of(context).size.width * 0.5,
+      //         ),
+      //         Container(
+      //           child: Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: <Widget>[
+      //               Row(
+      //                 children: <Widget>[
+      //                   Icon(
+      //                     Icons.calendar_today,
+      //                     size: 20,
+      //                   ),
+      //                   Text(_date),
+      //                 ],
+      //               ),
+      //             ],
+      //           ),
+      //           margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+      //           width: MediaQuery.of(context).size.width * 0.5,
+      //         ),
+      //         Container(
+      //           child: Text("Posted By: " + job['jobDetails']['employer']),
+      //           margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+      //         )
+      //       ], crossAxisAlignment: CrossAxisAlignment.start)
+      //     ],
+      //   ),
+      //   height: 140,
+      //   margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      //   decoration: BoxDecoration(
+      //       color: Color.fromRGBO(191, 241, 255, 1),
+      //       borderRadius: BorderRadius.only(
+      //           topLeft: Radius.circular(20),
+      //           bottomLeft: Radius.circular(20),
+      //           topRight: Radius.circular(20),
+      //           bottomRight: Radius.circular(20))),
+      // ),
+    );
+  }
+}
+
+Container _info (BuildContext context, DocumentSnapshot job, String date){
+  return(
+    Container(
+      height: MediaQuery.of(context).size.height * 0.17,
+      width: MediaQuery.of(context).size.width * 0.55,
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width * 0.55,
+            child: Text(job.data['jobDetails']['name'], textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.bold)),
+            margin: EdgeInsets.fromLTRB(35, 10, 0, 0),
+          ),
+          Container(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -79,8 +161,8 @@ class JobCard extends StatelessWidget {
                     ),
                   ],
                 ),
-                margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                width: MediaQuery.of(context).size.width * 0.5,
+                margin: EdgeInsets.fromLTRB(30, 10, 10, 10),
+                width: MediaQuery.of(context).size.width * 0.55,
               ),
               Container(
                 child: Row(
@@ -92,31 +174,37 @@ class JobCard extends StatelessWidget {
                           Icons.calendar_today,
                           size: 20,
                         ),
-                        Text(_date),
+                        Text(date),
                       ],
                     ),
                   ],
                 ),
-                margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                width: MediaQuery.of(context).size.width * 0.5,
+                margin: EdgeInsets.fromLTRB(35, 0, 10, 10),
+                width: MediaQuery.of(context).size.width * 0.55,
               ),
-              Container(
+               Container(
                 child: Text("Posted By: " + job['jobDetails']['employer']),
-                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                margin: EdgeInsets.fromLTRB(35, 0, 0, 0),
+                width:  MediaQuery.of(context).size.width * 0.55,
               )
-            ], crossAxisAlignment: CrossAxisAlignment.start)
-          ],
-        ),
-        height: 140,
-        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-        decoration: BoxDecoration(
-            color: Color.fromRGBO(191, 241, 255, 1),
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-                bottomRight: Radius.circular(20))),
+        ],
       ),
-    );
-  }
+      color: Color.fromRGBO(191, 241, 255, 1),
+      margin: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.4, MediaQuery.of(context).size.height * 0.015 + 5, 0, 0),
+    )
+  );
+}
+
+Container _pic (BuildContext context,DocumentSnapshot job){
+  return(
+    Container(
+      height: MediaQuery.of(context).size.height * 0.2,
+      width: MediaQuery.of(context).size.width *  0.4,
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+        child: Image(image: NetworkImage(job.data['jobDetails']['image']), fit: BoxFit.cover,)
+      ),
+      margin: EdgeInsets.fromLTRB(10, 5, 0, 0),
+    )
+  );
 }
