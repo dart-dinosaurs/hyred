5f450743edfa43d64294bd96ea708f900c03bd3a
skeshavaa <s.keshavaa@gmail.com>
2020-04-26 17:38:05 -0400

    replacing search

--- lib/screens/seeker-screens/history.dart
+++ lib/screens/seeker-screens/history.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:main/screens/seeker-screens/search.dart';
 import 'package:main/screens/widgets/history_card.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:main/screens/widgets/history_card.dart';
@@ -15,12 +14,15 @@ class History extends StatefulWidget {
 
 class _HistoryState extends State<History> {
   List<DocumentSnapshot> _jobs;
+  List<DocumentSnapshot> _employerData;
   bool _loading;
   String searchValue = "";
 
   @override
   void initState(){
     _loading = true;
+    _jobs = [];
+    _employerData = [];
     super.initState();
   }
 
@@ -28,9 +30,16 @@ class _HistoryState extends State<History> {
     this.setState(
       (){
         _jobs = newData;
-        _loading = false;
+        
       }
     );
+  }
+
+  void setEmployerData(List<DocumentSnapshot> employerData){
+    this.setState(() {
+      _employerData = employerData;
+      _loading = false;
+    });
   }
 
   void setSearch(String value) {
@@ -44,6 +53,9 @@ class _HistoryState extends State<History> {
 
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> _cards = [];
+
     Future<List<DocumentSnapshot>> setup() async {
       List<Future<DocumentSnapshot>> list = [];
       Provider.of<DocumentSnapshot>(context).data["listings"].forEach((ref) async {
@@ -51,26 +63,51 @@ class _HistoryState extends State<History> {
       });
       return await Future.wait(list);
     }
+
+    Future<List<DocumentSnapshot>> getEmployerData(List<DocumentSnapshot> list) async {
+      List<Future<DocumentSnapshot>> employerData = [];
+      list.forEach((job) async {
+        employerData.add(job.data['user'].get());
+      });
+      return await Future.wait(employerData);
+    }
+
+
     
     if (Provider.of<DocumentSnapshot>(context) == null) {
       return (Loading());
     } else {
       if(_loading){
         setup().then((list){
-        setData(list);
-      });
+          setData(list);
+        });
+        getEmployerData(_jobs).then((employerData) {
+          setEmployerData(employerData);
+        });
+        
         return Loading();
       } else {
-        List<Widget> _cards = [];
+        
         _jobs.reversed.forEach((doc) => {
           _cards.add(HistoryCard(doc)),
         });
-        print(_cards);
+        List<Widget> hits = [];
+
+        for (int i = 0; i < _jobs.length; i++){
+          if (_jobs[i].data['name'].toLowerCase().contains(searchValue.toLowerCase())
+            || _jobs[i].data['categories'].contains(searchValue.toLowerCase())
+            || _employerData[i].data['businessName'].toLowerCase().contains(searchValue.toLowerCase())
+            || _employerData[i].data['city'].toLowerCase().contains(searchValue.toLowerCase()) 
+          ){
+          hits.add(HistoryCard(_jobs[i]));
+          }
+        }
+
         return(
           ListView(children: <Widget>[
               SearchBar(controller: myController, onChange: (value) => {setSearch(value)}),
               searchValue != "" 
-                ? Search(searchValue, _jobs)
+                ? Column(children: hits)
                 : Column(children: _cards)
           ,
           ]
